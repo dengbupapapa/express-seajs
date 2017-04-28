d157228c1194282b6f80b8af057730cd87db61b6
dengbupapapa <meizhanghao1989@qq.com>
2017-04-28 15:45:02 +0800

    init

---
+++ dev/public/static/index/index.js
@@ -0,0 +1,6 @@
+define(function(require, exports, module) {
+    var a = require('../../static/common/js/module1/module1');
+    console.log(a)
+    a('holle module1');
+    console.log('holle fis3')
+});
